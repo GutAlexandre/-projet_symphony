21d031b837ddc97a71d44628a9a243923a89c9c6
root <root@docker.local>
2023-01-13 13:06:43 +0000

    src web perso

--- DOCKER-COMPOSE/src/index.php
+++ DOCKER-COMPOSE/src/index.php
@@ -1,6 +1,6 @@
 <!doctype html>
 <style>
-  body { text-align: center; padding: 150px;background-color:blue }
+  body { text-align: center; padding: 150px;background-color:red }
   h1 { text-align:center;font-size: 25px;color:white  }
   body { font: 16px Helvetica, sans-serif; color: #333; }
   article { display: block; text-align: left; width: 650px; margin: 0 auto; }
